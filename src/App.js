af515a24b3afdb4f116a12890399358d5084a103
Elvin <aliyeve669@gmail.com>
2022-11-22 10:07:37 +0400

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import Contacts from './Components/Contacts/Contacts';
+import UserCV from './Components/UserCV/UserCV';
+import PersonalInfo from './Components/PersonalInfo/PersonalInfo';
+import Experience from './Components/Experience/Experience';
+function App() {
+  return (
+    <>
+      <Header/>
+      <Contacts/>
+      <UserCV/>
+      <PersonalInfo/>
+      <Experience/>
+    </>
+  );
+}
+
+export default App;
